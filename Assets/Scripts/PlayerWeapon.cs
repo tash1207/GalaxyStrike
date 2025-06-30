f749ae289d96e6fb622e2935b612ae5f0d6151ab
Tasha Hankewych <thankewych@gmail.com>
2025-06-30 00:17:24 -0500

    Set up a UI crosshair and target point object that follow the mouse position

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,15 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 200f;
 
     bool isFiring = false;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
 
     void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
+        MoveTargetPoint();
     }
 
     public void OnFire(InputValue value)
@@ -22,7 +33,19 @@ public class PlayerWeapon : MonoBehaviour
         foreach (GameObject laser in lasers)
         {
             var emissionModule = laser.GetComponent<ParticleSystem>().emission;
-            emissionModule.enabled = isFiring;   
+            emissionModule.enabled = isFiring;
         }
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
+    }
+
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition =
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
     }
 }
