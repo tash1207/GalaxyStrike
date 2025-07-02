96b5be66c17c295d6d2c2ae02588a68c68250cb5
Tasha Hankewych <thankewych@gmail.com>
2025-07-02 12:45:49 -0500

    Set up different amounts of hit points for each enemy ship

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem destroyedShipVFX;
+    [SerializeField] int hitPoints = 4;
 
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedShipVFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyedShipVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
